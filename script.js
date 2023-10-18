33b5dbe7ec72880b17156971fc29e0c7789ab945
molano5858 <molano5858@gmail.com>
2023-10-18 18:05:25 -0500

    make reset functionality and select valid date functionality

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
 
+let countdownActive;
+
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
@@ -23,36 +25,37 @@ dateEl.setAttribute("min", today);
 
 // update DOM with date values
 function updateDom() {
-  //const now = new Date().getTime();
-  const tzOffset = -300;
-  const now = new Date();
-  const colombiaTime = new Date(now.getTime() + tzOffset * 60 * 1000);
+  countdownActive = setInterval(() => {
+    //const now = new Date().getTime();
+    const tzOffset = -300;
+    const now = new Date();
+    const colombiaTime = new Date(now.getTime() + tzOffset * 60 * 1000);
 
-  // difference between today and the date selected
-  // const distance = countdownValue - now;
-  const distance = countdownValue - colombiaTime;
-  const daysRemaining = Math.floor(distance / day);
-  const hoursRemaining = Math.floor((distance % day) / hour);
-  const minutesRemaining = Math.floor((distance % hour) / minute);
-  const secondsRemaining = Math.floor((distance % minute) / second);
-  console.log({
-    daysRemaining,
-    hoursRemaining,
-    minutesRemaining,
-    secondsRemaining,
-  });
+    // difference between today and the date selected
+    // const distance = countdownValue - now;
+    const distance = countdownValue - colombiaTime;
+    const daysRemaining = Math.floor(distance / day);
+    const hoursRemaining = Math.floor((distance % day) / hour);
+    const minutesRemaining = Math.floor((distance % hour) / minute);
+    const secondsRemaining = Math.floor((distance % minute) / second);
+    console.log({
+      daysRemaining,
+      hoursRemaining,
+      minutesRemaining,
+      secondsRemaining,
+    });
+    // Populating UI
+    countdownElTitle.textContent = `${countdownTitle}`;
+    timeElements[0].textContent = `${daysRemaining}`;
+    timeElements[1].textContent = `${hoursRemaining}`;
+    timeElements[2].textContent = `${minutesRemaining}`;
+    timeElements[3].textContent = `${secondsRemaining}`;
 
-  // Populating UI
-  countdownElTitle.textContent = `${countdownTitle}`;
-  timeElements[0].textContent = `${daysRemaining}`;
-  timeElements[1].textContent = `${hoursRemaining}`;
-  timeElements[2].textContent = `${minutesRemaining}`;
-  timeElements[3].textContent = `${secondsRemaining}`;
-
-  // Hide input form
-  inputContainer.hidden = true;
-  // show countdown
-  countdownEl.hidden = false;
+    // Hide input form
+    inputContainer.hidden = true;
+    // show countdown
+    countdownEl.hidden = false;
+  }, 1000);
 }
 
 // take values form the form
@@ -60,11 +63,29 @@ function updateCountdown(event) {
   event.preventDefault();
   countdownTitle = event.srcElement[0].value;
   countdownDate = event.srcElement[1].value;
-  // get number version of current date, update DOM
-  countdownValue = new Date(countdownDate).getTime();
-  console.log(countdownValue);
-  updateDom();
+  // checking there is a valid date
+  if (countdownDate === "") {
+    alert("Please, select the date for the countdown");
+  } else {
+    // get number version of current date, update DOM
+    countdownValue = new Date(countdownDate).getTime();
+    console.log(countdownValue);
+    updateDom();
+  }
+}
+
+// Reset app functionality
+function resetApp() {
+  // hide countdown and show inputs
+  inputContainer.hidden = false;
+  countdownEl.hidden = true;
+  // Stop interval
+  clearInterval(countdownActive);
+  // reset values
+  let countdownTitle = "";
+  let countdownDate = "";
 }
 
 // Event listeners
 countdownForm.addEventListener("submit", updateCountdown);
+countdownButton.addEventListener("click", resetApp);
